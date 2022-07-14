e8fb6cb9a81f04016b8a00b9e644cb5d9995adf9
DzmitryRazumejchyk <108740076+dimarazumejchyk@users.noreply.github.com>
2022-07-14 16:07:27 +0300

    Commit

--- src/com/noirix/homework2/Main.java
+++ src/com/noirix/homework2/Main.java
@@ -18,24 +18,5 @@ public class Main {
         car5.setOwnerFullName("Vova_Selivanov");
         System.out.println(car5.getOwnerFullName());
 
- //       System.out.println(car1.toString());
- //       System.out.println(car1.hashCode());
- //       double cost1 = car1.cost100(2.44, 6);
- //       System.out.println("стоимость 100 км пути " + cost1 + " руб");
- //       car1.someMethod();
-//
- //       System.out.println(car2.toString());
- //       System.out.println(car2.hashCode());
- //       double cost2 = car2.cost100(2.44, 5);
- //       System.out.println("стоимость 100 км пути " + cost2 + " руб");
- //       car2.someMethod();
-//
- //       System.out.println(car1.equals(car2));
-//
- //       System.out.println(car3.toString());
- //       System.out.println(car3.hashCode());
- //       double cost3 = car3.cost100(2.48, 8);
- //       System.out.println("стоимость 100 км пути " + cost3 + " руб");
- //       car3.someMethod();
     }
 }
